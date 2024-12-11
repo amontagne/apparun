aceffe7b41159b282f65fd6d52775529347ee305
Maxime PERALTA <maxime.peralta@cea.fr>
2024-12-11 17:54:59 +0100

    First public version of Appa Run

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="apparun",
+    version="0.2.0",
+    author="Maxime Peralta",
+    author_email="maxime.peralta@cea.fr",
+    description="Appa Run is a package to execute impact models produced by Appa Build",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "apparun=app.cli.main:cli_app",
+        ],
+    },
+)
